2ec108d2893dfdf7455a57801b68ef179bc24b17
tasha <tasha137@mail.ru>
2026-09-18 22:05:26 +0700

    feat: add 8 kyu or Find Max and Min kata solution

---
+++ 8kyu/Find Maximum and Minimum Values of a List.py
@@ -0,0 +1,34 @@
+"""
+Kata: Find Maximum and Minimum Values of a List (8 kyu)
+Link: https://www.codewars.com/kata/577a98a6ae28071780000989
+
+Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language )
+that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
+Each function returns one number.
+
+Examples (Input -> Output)
+* [4,6,2,1,9,63,-134,566]         -> max = 566, min = -134
+* [-52, 56, 30, 29, -54, 0, -110] -> min = -110, max = 56
+* [42, 54, 65, 87, 0]             -> min = 0, max = 87
+* [5]                             -> min = 5, max = 5
+Notes
+You may consider that there will not be any empty arrays/vectors.
+"""
+
+
+def minimum(arr):
+    min_number = arr[0]  # сначала считаем что первое число самое маленькое
+
+    for number in arr:  # затем проходим по всем числам, если нашли число меньше текущего минимума - заменяем минимум
+        if number < min_number:
+            min_number = number
+    return min_number
+
+
+def maximum(arr):  # для максимума тоже самое
+    max_number = arr[0]
+
+    for number in arr:
+        if number > max_number:
+            max_number = number
+    return max_number
